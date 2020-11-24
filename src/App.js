b7fc85c8391b52b26d52838049264f67f6db38a5
jim@pulamusic.com <jim@pulamusic.com>
2020-11-24 11:15:46 -0500

    can't remember

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>poop</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>fart</p>
       </header>
     </div>
   );
